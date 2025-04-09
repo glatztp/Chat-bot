f7b9d14c0cff7bf844b8ed787b91692773b77893
Gabriel Fellipe Glatz <gabrielfellipeglatz@gmail.com>
2025-04-08 21:45:20 -0300

    feat: n8n

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Chat from "@/components/Chat";
+import './lib/n8n-chat'
 
 export default function Home() {
   return (
---
+++ src/lib/n8n-chat.ts
@@ -0,0 +1,5 @@
+import { createChat } from '@n8n/chat'
+
+createChat({
+  webhookUrl: 'http://localhost:3000/api/chat'
+})
